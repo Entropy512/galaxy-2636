13b277bcc3d85dc6dbe084303b93d902c4bb19a8
Wei Ni <wni@nvidia.com>
2011-10-25 17:36:21 +0000

    rtc: tps6586x: Fix for proper wakealarm
    In the driver probe function, call device_init_wakeup() before
    registering rtc device, so that wakealarm sysfs will be created.
    
    BUG=889820
    
    Change-Id: I957fdffb02cd7e43dd96db0794e2e9f8dc7bd3ee
    Signed-off-by: Wei Ni <wni@nvidia.com>
    Reviewed-on: http://git-master/r/58937
    Reviewed-by: Allen Martin <amartin@nvidia.com>

--- drivers/rtc/rtc-tps6586x.c
+++ drivers/rtc/rtc-tps6586x.c
@@ -364,6 +364,8 @@ static int __devinit tps6586x_rtc_probe(struct platform_device *pdev)
 	rtc->epoch_start = mktime(epoch->year, epoch->month, epoch->day,
 				  epoch->hour, epoch->min, epoch->sec);
 
+	device_init_wakeup(&pdev->dev, 1);
+
 	rtc->rtc = rtc_device_register("tps6586x-rtc", &pdev->dev,
 				       &tps6586x_rtc_ops, THIS_MODULE);
 
@@ -408,7 +410,6 @@ static int __devinit tps6586x_rtc_probe(struct platform_device *pdev)
 			dev_warn(&pdev->dev, "unable to request IRQ(%d)\n", rtc->irq);
 			rtc->irq = -1;
 		} else {
-			device_init_wakeup(&pdev->dev, 1);
 			enable_irq_wake(rtc->irq);
 			disable_irq(rtc->irq);
 		}
@@ -430,6 +431,7 @@ static int __devinit tps6586x_rtc_probe(struct platform_device *pdev)
 fail:
 	if (!IS_ERR_OR_NULL(rtc->rtc))
 		rtc_device_unregister(rtc->rtc);
+	device_init_wakeup(&pdev->dev, 0);
 	kfree(rtc);
 	return err;
 }
